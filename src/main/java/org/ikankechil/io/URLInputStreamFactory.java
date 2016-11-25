8e51fd81dd976807680b4a92a103e1470fd696f1
dkcm <daniel.kuan@gmail.com>
2016-11-25 16:38:44 +0800

    Support OkHttp v2 and v3.

--- src/main/java/org/ikankechil/io/URLInputStreamFactory.java
+++ src/main/java/org/ikankechil/io/URLInputStreamFactory.java
@@ -1,7 +1,7 @@
 /**
- * URLInputStreamFactory.java  v0.2  4 November 2014 5:16:25 PM
+ * URLInputStreamFactory.java  v0.4  4 November 2014 5:16:25 PM
  *
- * Copyright © 2014-2015 Daniel Kuan.  All rights reserved.
+ * Copyright © 2014-2016 Daniel Kuan.  All rights reserved.
  */
 package org.ikankechil.io;
 
@@ -24,80 +24,108 @@ import org.slf4j.LoggerFactory;
  * <p>
  *
  * @author Daniel Kuan
- * @version 0.2
+ * @version 0.4
  */
 public final class URLInputStreamFactory {
+  // TODO
+  // 1. evaluate cronet, jersey
+  // 2. support Brotli ("br"), SDCH ("sdch")?
+  // 3. [COMPLETED] v0.4 support cookies
 
-  private static boolean      isTransparentEncoding;
+  private static boolean        isTransparentEncoding;
 
-  private static final String OK_HTTP_CLIENT    = "com.squareup.okhttp.OkHttpClient";
-  private static final String OK_URL_FACTORY    = "com.squareup.okhttp.OkUrlFactory";
+  private static final String[] OK_HTTP_CLIENTS      = { "okhttp3.OkHttpClient",
+                                                         "com.squareup.okhttp.OkHttpClient" };
+  private static final String[] OK_URL_FACTORIES     = { "okhttp3.OkUrlFactory",
+                                                         "com.squareup.okhttp.OkUrlFactory" };
 
-  private static final String FACTORY           = "factory";
-  private static final String HEADERS           = "headers";
+  private static final String   FACTORY              = "factory";
+  private static final String   HEADERS              = "headers";
 
-  // HTTP-related
-  private static final String GZIP              = "gzip";
-  private static final String DEFLATE           = "deflate";
-  private static final String SCHEMES           = GZIP + ',' + DEFLATE;
-  private static final String CHUNKED           = "chunked";
-  private static final String ACCEPT_ENCODING   = "Accept-Encoding";
-  private static final String CONTENT_ENCODING  = "Content-Encoding";
-  private static final String TRANSFER_ENCODING = "Transfer-Encoding";
-  private static final String CONTENT_RANGE     = "Content-Range";
+  // HTTP encoding schemes
+  private static final String   GZIP                 = "gzip";
+  private static final String   DEFLATE              = "deflate";
+  private static final String   SCHEMES              = GZIP + ',' + DEFLATE;
+  private static final String   CHUNKED              = "chunked";
 
-  private static final int    BUFFER_SIZE       = 65536;
-  private static final String URL_CONNECTION    = "URLConnection {}: {}";
+  // HTTP headers
+  private static final String   ACCEPT_ENCODING      = "Accept-Encoding";
+  private static final String   CONTENT_ENCODING     = "Content-Encoding";
+  private static final String   TRANSFER_ENCODING    = "Transfer-Encoding";
+  private static final String   CONTENT_RANGE        = "Content-Range";
+  private static final String   COOKIE               = "Cookie";
 
-  private static final Logger logger            = LoggerFactory.getLogger(URLInputStreamFactory.class);
+  private static final int      BUFFER_SIZE          = 65536;
+  private static final String   TRANSPARENT_ENCODING = "Transparent Encoding: {}";
+  private static final String   URL_CONNECTION       = "URLConnection {}: {}";
+
+  private static final Logger   logger               = LoggerFactory.getLogger(URLInputStreamFactory.class);
 
   private URLInputStreamFactory() { /* disallow instantiation */ }
 
   static { // load OkHttp client if available and possible, else default to JRE
-    try {
-      logger.info("Looking for: {} and {}", OK_HTTP_CLIENT, OK_URL_FACTORY);
-      final Class<?> okhc = Class.forName(OK_HTTP_CLIENT);
-      final Class<?> okuf = Class.forName(OK_URL_FACTORY);
-      final Constructor<?> okufCtor = okuf.getConstructor(okhc);
-
+    for (int i = 0; i < OK_HTTP_CLIENTS.length; ++i) {
+      final String okHttpClient = OK_HTTP_CLIENTS[i];
+      final String okUrlFactory = OK_URL_FACTORIES[i];
       try {
-        URL.setURLStreamHandlerFactory((URLStreamHandlerFactory) okufCtor.newInstance(okhc.newInstance()));
+        logger.info("Looking for: {} and {}", okHttpClient, okUrlFactory);
+        final Class<?> okhc = Class.forName(okHttpClient);
+        final Class<?> okuf = Class.forName(okUrlFactory);
+        final Constructor<?> okufCtor = okuf.getConstructor(okhc);
 
-        isTransparentEncoding = true;
-        logger.info("Using OkHttp");
+        try {
+          URL.setURLStreamHandlerFactory((URLStreamHandlerFactory) okufCtor.newInstance(okhc.newInstance()));
+
+          isTransparentEncoding = true;
+          logger.info("Using OkHttp");
+          break;
+        }
+        catch (final Error e) {
+          final Field field = URL.class.getDeclaredField(FACTORY);
+          field.setAccessible(true);
+          final String factory = field.get(null).getClass().getName();
+          logger.error("URLStreamHandlerFactory already defined: {}", factory);
+          break;
+        }
       }
-      catch (final Error e) {
-        final Field field = URL.class.getDeclaredField(FACTORY);
-        field.setAccessible(true);
-        final String factory = field.get(null).getClass().getName();
-        logger.error("URLStreamHandlerFactory already defined: {}", factory);
+      catch (final ReflectiveOperationException | // super class of ClassNotFoundException, NoSuchMethodException, etc.
+                   SecurityException |
+                   IllegalArgumentException e) {
+        logger.debug("{} and {} unavailable", okHttpClient, okUrlFactory);
+        if (i == OK_HTTP_CLIENTS.length - 1) {
+          logger.warn("OkHttp unavailable.  Defaulting to JRE");
+        }
       }
-    }
-    catch (final ReflectiveOperationException |
-                 SecurityException |
-                 IllegalArgumentException e) {
-      logger.warn("OkHttp unavailable: {}", e.getMessage(), e);
-      logger.info("Defaulting to JRE");
     }
   }
 
   /**
-   * Creates an input stream that reads from <code>source</code>.
+   * Creates an input stream that reads from a <code>URL</code>.
    *
-   * @param source
-   * @return an input stream that reads from <code>source</code>
+   * @param source the source <code>URL</code>
+   * @return an input stream that reads from a <code>URL</code>
    * @throws IOException
    *           if an I/O error occurs while creating the input stream
    */
   public static final InputStream newInputStream(final URL source) throws IOException {
+    return newInputStream(source, null);
+  }
+
+  public static final InputStream newInputStream(final URL source, final String cookie) throws IOException {
     final URLConnection connection = source.openConnection();
     if (!isTransparentEncoding) {
       // does not work well with OkHttp
       connection.setRequestProperty(ACCEPT_ENCODING, SCHEMES);
       connection.setRequestProperty(TRANSFER_ENCODING, CHUNKED);
     }
-    logger.debug("Transparent Encoding: {}", isTransparentEncoding);
+    logger.debug(TRANSPARENT_ENCODING, isTransparentEncoding);
 
+    // set cookie
+    if (cookie != null) {
+      connection.setRequestProperty(COOKIE, cookie);
+    }
+
+    // decode by content encoding
     final String contentEncoding = connection.getHeaderField(CONTENT_ENCODING);
     logger.debug(URL_CONNECTION, CONTENT_ENCODING, contentEncoding);
     logger.debug(URL_CONNECTION, TRANSFER_ENCODING, connection.getHeaderField(TRANSFER_ENCODING));
--- src/test/java/org/ikankechil/io/URLInputStreamFactoryTest.java
+++ src/test/java/org/ikankechil/io/URLInputStreamFactoryTest.java
@@ -1,10 +1,20 @@
 /**
- * URLInputStreamFactoryTest.java v0.1 7 April 2015 10:06:58 AM
+ * URLInputStreamFactoryTest.java  v0.3  7 April 2015 10:06:58 AM
  *
- * Copyright © 2015 Daniel Kuan.  All rights reserved.
+ * Copyright © 2015-2016 Daniel Kuan.  All rights reserved.
  */
 package org.ikankechil.io;
 
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.InflaterInputStream;
+
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -14,17 +24,95 @@ import org.junit.rules.ExpectedException;
  * <p>
  *
  * @author Daniel Kuan
- * @version 0.1
+ * @version 0.3
  */
 public class URLInputStreamFactoryTest {
 
+  private static boolean         isTransparentEncoding;
+
+  private static final String    URL             = "http://httpbin.org/";
+  private static final String    GZIP_URL        = "http://httpbin.org/gzip";
+  private static final String    DEFLATE_URL     = "http://httpbin.org/deflate";
+
+  private static final String    OK_URL_FACTORY  = "com.squareup.okhttp.OkUrlFactory";
+  private static final String    OK_URL_FACTORY3 = "okhttp3.OkUrlFactory";
+
+//  private static final String    COOKIE          = "Cookie";
+
   @Rule
-  public final ExpectedException thrown = ExpectedException.none();
+  public final ExpectedException thrown          = ExpectedException.none();
+
+  static {
+    try {
+      final Field field = URLInputStreamFactory.class.getDeclaredField("isTransparentEncoding");
+      field.setAccessible(true);
+      isTransparentEncoding = field.getBoolean(URLInputStreamFactory.class);
+    }
+    catch (NoSuchFieldException |
+           SecurityException |
+           IllegalArgumentException |
+           IllegalAccessException e) {
+      isTransparentEncoding = false;
+    }
+  }
 
   @Test
-  public void cannotCreateInputStreamWithNullURL() throws Exception {
+  public void cannotCreateInputStreamWithNullURL() throws IOException {
     thrown.expect(NullPointerException.class);
     URLInputStreamFactory.newInputStream(null);
+  }
+
+  @Test
+  public void newGzipEncodedInputStream() throws IOException {
+    try (final InputStream actual = URLInputStreamFactory.newInputStream(new URL(GZIP_URL))) {
+      if (isTransparentEncoding) {
+        assertFalse(actual instanceof GZIPInputStream);
+      }
+      else {
+        assertTrue(actual instanceof GZIPInputStream);
+      }
+    }
+  }
+
+  @Test
+  public void newDeflateEncodedInputStream() throws IOException {
+    try (final InputStream actual = URLInputStreamFactory.newInputStream(new URL(DEFLATE_URL))) {
+      assertTrue(actual instanceof InflaterInputStream);
+    }
+  }
+
+  @Test
+  public void newUnencodedInputStream() throws IOException {
+    try (final InputStream actual = URLInputStreamFactory.newInputStream(new URL(URL))) {
+      assertFalse(actual instanceof GZIPInputStream);
+      assertFalse(actual instanceof InflaterInputStream);
+    }
+  }
+
+  @Test
+  public void transparentEncodingWhenUsingOkHttp() {
+    try {
+      Class.forName(OK_URL_FACTORY3);
+      assertTrue(isTransparentEncoding);
+    }
+    catch (final ClassNotFoundException cnfE) {
+      try {
+        Class.forName(OK_URL_FACTORY);
+        assertTrue(isTransparentEncoding);
+      }
+      catch (final ClassNotFoundException cnfE2) {
+        assertFalse(isTransparentEncoding);
+      }
+    }
+  }
+
+  @Ignore
+  @Test
+  public void nullCookiesNotSet() throws IOException {
+    final URL url = new URL(URL);
+    try (final InputStream actual = URLInputStreamFactory.newInputStream(url, null)) {
+//      final String expected = url.openConnection().getHeaderField(COOKIE);
+    }
   }
 
 }
